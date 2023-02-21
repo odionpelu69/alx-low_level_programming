a8f86d2fd1630f5e16f7c63dee7365a56fdacfb1
odionpelu69 <odionpelu69@gmail.com>
2023-02-21 02:24:29 -0800

    hello_world.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main.Entry point
+ *
+ * return always 0(successful)
+ */
+int main (void)
+ (
+  puts('\' 'Programming is like build  ing a multilingual puzzle') 
+  return (0);
+ ); 
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main. entry point
+ *
+ * return always 0 (successfull)
+ */
+int main (void)
+(
+ printf ('with proper grammar, but th e outcome is a piece of art');
+ return (0);
+)
